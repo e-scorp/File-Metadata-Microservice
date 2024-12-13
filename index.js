28d18c5b36d8d0f72179e6f550aaad712cf6ee3a
Yash doe <yasgavande78@gmail.com>
2024-12-13 12:26:41 +0000

    update file-metadata-microservice

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+import "dotenv/config.js"
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+//set up file upload using Multer
+const upload = multer({ dest: 'uploads/' })
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//api to handle file uploads and return metadata
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" })
+  }
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  })
+})
 
 
 const port = process.env.PORT || 3000;
